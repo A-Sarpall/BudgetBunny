1b248adce474752b17632bfd28b53e958c135d3e
Wyatt-Gold <103477172+Wyatt-Gold@users.noreply.github.com>
2023-10-21 17:12:49 -0500

    Update main.py
    sdf

--- main.py
+++ main.py
@@ -3,6 +3,26 @@
 #cd desktop
 #streamlit run main.py
 import streamlit as st
+import requests
+import webbrowser
 
-if st.button("+ Add Bank Account"):
-    st.markdown("https://www.coolmathgames.com/")
+url = "https://www.coolmathgames.com/"
+
+st.markdown(
+    """
+    <style>
+    .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Centered button
+with st.markdown("<div class='centered'>"):
+    if st.button("+ Add Bank Account"):
+        webbrowser.open(url)
