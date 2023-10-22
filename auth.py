0044af00ef17c8bdc5b3582427703a6c8f2ea72d
Ahan Saripalli <96845815+A-Sarpall@users.noreply.github.com>
2023-10-22 10:51:18 -0500

    Final auth.py

--- auth.py
+++ auth.py
@@ -1,18 +1,56 @@
 import streamlit as st
-import requests 
+import openai
+import requests
+from streamlit.components.v1 import html
+
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+    
+
+# Define app states
+states = {"Login": 0, "Home": 1}
+
+# Initialize the state
+state = states["Login"]
 
 # Streamlit app code (input fields, buttons, etc.)
-st.title("Firebase Authentication Example")
+st.set_page_config(initial_sidebar_state= "collapsed")
+st.title("BudgetBunny Login")
 email = st.text_input("Email")
 password = st.text_input("Password", type="password")
 
 # Firebase Web API key
-api_key = "AIzaSyBiGNTXBSrKniaEE0tkn-ROUo6VA7M2S54"
+api_key = "AIzaSyBiGNTXBSrKniaEE0tkn-ROUo6VA7M2S54"  # Replace with your Firebase API key
 
 # Firebase Authentication API endpoints
 base_url = "https://identitytoolkit.googleapis.com/v1"
 signup_url = f"{base_url}/accounts:signUp?key={api_key}"
 signin_url = f"{base_url}/accounts:signInWithPassword?key={api_key}"
+
+# Create a container for content
+content_container = st.container()
 
 # Sign-up function
 def signup(email, password):
@@ -25,6 +63,7 @@ def signup(email, password):
     result = response.json()
     if "idToken" in result:
         st.success("Sign-up successful!")
+        
     else:
         st.error(result["error"]["message"])
 
@@ -39,12 +78,17 @@ def signin(email, password):
     result = response.json()
     if "idToken" in result:
         st.success("Sign-in successful!")
+        nav_page("AI")
+        return states["Home"]
     else:
         st.error(result["error"]["message"])
+        return states["Login"]
 
-if st.button("Sign up"):
-    signup(email, password)
-
-if st.button("Sign in"):
-    signin(email, password)
-
+if state == states["Login"]:
+    if st.button("Sign up"):
+        signup(email, password)
+        # Clear the content container to remove previous content
+    if st.button("Sign in"):
+        state = signin(email, password)
+        # Clear the content container to remove previous content
+        content_container.empty()
