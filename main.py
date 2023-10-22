b93e6958553f83f3a4d6d83a735dae38465bb749
CadenWonzer <97636285+CadenWonzer@users.noreply.github.com>
2023-10-21 20:41:22 -0500

    UI Start

--- main.py
+++ main.py
@@ -22,6 +22,24 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+#logo
+# Create a Streamlit layout with two columns
+col1, col2, col3 = st.columns([0.5, 2, 4], gap = "small")
+
+# Add an image to the left column
+with col1:
+    st.image("budgetbunny.png")
+
+# Add content to the right column
+with col2:
+    st.title("BudgetBunny")
+
+with col3:
+    tab1, tab2 = st.tabs(["Budgeting", "Trading"])
+
+    with tab2:
+        st.write("feature is under construction")
+
 # Centered button
 with st.markdown("<div class='centered'>"):
     if st.button("+ Add Bank Account"):
